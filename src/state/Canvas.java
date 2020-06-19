a893f459a959cbe1c3db1149d5216436d5bd593c
Nguyen Phuc Thien An <npta97@gmail.com>
2020-06-19 09:38:20 +0700

    Add State pattern

---
+++ src/state/Canvas.java
@@ -0,0 +1,22 @@
+package state;
+
+public class Canvas {
+
+    private Tool currentTool;
+
+    public Tool getCurrentTool() {
+        return currentTool;
+    }
+
+    public void setCurrentTool(Tool currentTool) {
+        this.currentTool = currentTool;
+    }
+
+    public void mouseDown() {
+        currentTool.mouseDown();
+    }
+
+    public void mouseUp() {
+        currentTool.mouseUp();
+    }
+}
---
+++ src/state/Tool.java
@@ -0,0 +1,8 @@
+package state;
+
+public interface Tool {
+
+    void mouseDown();
+
+    void mouseUp();
+}
